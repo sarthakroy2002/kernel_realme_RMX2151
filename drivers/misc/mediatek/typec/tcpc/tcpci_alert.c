72fd3717171a1e42e56b6f7fdda6c0aac3c168de
Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
2020-01-20 20:38:22 +0800

    [ALPS04936057] PMIC: fix mt6360 pd vsafe0v irq missing
    [Detail]
    fix not to clear vendor define irq when irq masked
    for pd compliance test TD.4.7.2
    
    MTK-Commit-Id: ff3e327e347348b5ac3c47cda9960c884a682b2c
    
    Change-Id: I0d3921a407223c68230bb1a6f67f46ee1cf13a65
    Signed-off-by: Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
    CR-Id: ALPS04936057
    Feature: Power Management

--- drivers/misc/mediatek/typec/tcpc/tcpci_alert.c
+++ drivers/misc/mediatek/typec/tcpc/tcpci_alert.c
@@ -353,13 +353,13 @@ static inline int __tcpci_alert(struct tcpc_device *tcpc_dev)
 		TCPC_INFO("Alert:0x%04x\r\n", alert_status);
 #endif /* CONFIG_USB_PD_DBG_ALERT_STATUS */
 
+	alert_status &= alert_mask;
+
 	tcpci_alert_status_clear(tcpc_dev,
 		alert_status & (~TCPC_REG_ALERT_RX_MASK));
 
 	if (tcpc_dev->typec_role == TYPEC_ROLE_UNKNOWN)
 		return 0;
-
-	alert_status &= alert_mask;
 
 	if (alert_status & TCPC_REG_ALERT_EXT_VBUS_80)
 		alert_status |= TCPC_REG_ALERT_POWER_STATUS;
--- drivers/misc/mediatek/typec/tcpc/tcpci_alert_v20.c
+++ drivers/misc/mediatek/typec/tcpc/tcpci_alert_v20.c
@@ -373,13 +373,13 @@ static inline int __tcpci_alert(struct tcpc_device *tcpc_dev)
 		TCPC_INFO("Alert:0x%04x\r\n", alert_status);
 #endif /* CONFIG_USB_PD_DBG_ALERT_STATUS */
 
+	alert_status &= alert_mask;
+
 	tcpci_alert_status_clear(tcpc_dev,
 		alert_status & (~TCPC_REG_ALERT_RX_MASK));
 
 	if (tcpc_dev->typec_role == TYPEC_ROLE_UNKNOWN)
 		return 0;
-
-	alert_status &= alert_mask;
 
 #ifdef CONFIG_USB_POWER_DELIVERY
 	if (tcpc_dev->pd_transmit_state == PD_TX_STATE_WAIT_HARD_RESET) {
